c1b60813129ee18114d0551e3d0877b7f7116ee5
Steve Gehrman <sgehrman@gmail.com>
2021-03-26 00:20:51 -0700

    fixed bug, didn't work if you don't pass controlr

--- lib/src/expandable_theme_notifier.dart
+++ lib/src/expandable_theme_notifier.dart
@@ -4,47 +4,25 @@ import 'package:flutter/material.dart';
 
 class ExpandableNotifier extends StatefulWidget {
   final ExpandableController controller;
-  final bool initialExpanded;
   final Widget child;
 
   const ExpandableNotifier({
     Key key,
-    this.controller,
-    this.initialExpanded,
+    @required this.controller,
     @required this.child,
-  })  : assert(!(controller != null && initialExpanded != null)),
-        super(key: key);
+  }) : super(key: key);
 
   @override
   _ExpandableNotifierState createState() => _ExpandableNotifierState();
 }
 
 class _ExpandableNotifierState extends State<ExpandableNotifier> {
-  ExpandableController controller;
   ExpandableThemeData theme;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = widget.controller ??
-        ExpandableController(initialExpanded: widget.initialExpanded ?? false);
-  }
-
-  @override
-  void didUpdateWidget(ExpandableNotifier oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (widget.controller != oldWidget.controller &&
-        widget.controller != null) {
-      setState(() {
-        controller = widget.controller;
-      });
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     final cn = ExpandableControllerNotifier(
-        controller: controller, child: widget.child);
+        controller: widget.controller, child: widget.child);
     return theme != null
         ? ExpandableThemeNotifier(themeData: theme, child: cn)
         : cn;
